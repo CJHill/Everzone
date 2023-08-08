5eb0f7788ef18544fa47ef9df34543b929f3f387
CJHill <up2014114@myport.ac.uk>
2023-08-08 13:10:41 +0100

    Initial commit

---
+++ Source/Everzone/EverzoneGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EverzoneGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EVERZONE_API AEverzoneGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
