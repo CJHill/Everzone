cd72b7c05acc7db20470c80db68b86607558f20e
CJHill <up2014114@myport.ac.uk>
2023-08-23 16:51:55 +0100

    Created weapon mesh, collision sphere and blueprint

--- Source/Everzone/Weapon/Weapon.h
+++ Source/Everzone/Weapon/Weapon.h
@@ -6,6 +6,15 @@
 #include "GameFramework/Actor.h"
 #include "Weapon.generated.h"
 
+UENUM(BlueprintType)
+enum class EWeaponState : uint8
+{
+	EWS_Initial UMETA(DisplayName = "Initial State"),
+	EWS_Equipped UMETA(DisplayName = "Equipped"),
+	EWS_Dropped UMETA(DisplayName = "Dropped"),
+
+	EWS_MAX UMETA(DisplayName = "DefaultMAX")
+};
 UCLASS()
 class EVERZONE_API AWeapon : public AActor
 {
@@ -19,7 +28,14 @@ protected:
 	
 	virtual void BeginPlay() override;
 private:
-	USkeletalMesh* WeaponMesh;
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+		USkeletalMeshComponent* WeaponMesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+		class USphereComponent* AreaSphere;
+	UPROPERTY(VisibleAnywhere)
+	EWeaponState WeaponState;
+
 public:	
 	
 	
